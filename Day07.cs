2a13569d1cfc708b24badf7b9279be12db3840c9
Stefano Maffeis <stefano.maffeis@gmail.com>
2023-12-07 10:01:23 +0100

    day 7

---
+++ Day07.cs
@@ -0,0 +1,114 @@
+namespace advent_of_code_2022;
+
+public static class Day07
+{
+    public const string TestFileName = "Day07_test";
+    public const string ProductionFileName = "Day07";
+
+    public static int Part1(List<string> input)
+    {
+        var hands = input.Select(it => new Hand(it)).OrderBy(it => it).ToList();
+
+        return hands.OrderBy(it => it).Select((hand, i) => hand.Bet * (i + 1)).Sum();
+    }
+
+    public static int Part2(List<string> input)
+    {
+        var hands = input.Select(it => new Hand(it, true)).ToList();
+
+        return hands.OrderBy(it => it).Select((hand, i) => hand.Bet * (i + 1)).Sum();
+    }
+
+    public class Hand : IComparable<Hand>
+    {
+        public Hand(string raw, bool useJoker = false)
+        {
+            UseJoker = useJoker;
+            var rawSplit = raw.Split(' ');
+            Bet = int.Parse(rawSplit[1]);
+            Cards = rawSplit[0].Select(it => new Card(it, UseJoker && it == 'J')).ToList();
+        }
+
+        private bool UseJoker { get; }
+
+        public int Bet { get; }
+        private List<Card> Cards { get; }
+
+        private bool IsFiveOfKind => CompareUsingJoker(5);
+        private bool IsFourOfKind => CompareUsingJoker(4);
+
+        private bool IsFullHouse => TwoOfKind && GroupedCard.Count(it => !it.Key.IsJoker) == 2;
+
+        private bool ThreeOfKind => CompareUsingJoker(3);
+
+        private bool TwoOfKind => GroupedCard.Any(it => !it.Key.IsJoker && it.Value == 2);
+        private bool TwoPairs => GroupedCard.Count(it => !it.Key.IsJoker && it.Value == 2) == 2;
+
+        private bool OnePair => GroupedCard.Count(it => !it.Key.IsJoker && it.Value == 2) == 1 || CountJokers == 1;
+        private int CountJokers => UseJoker ? Cards.Count(it => it.IsJoker) : 0;
+
+        public int HandValue
+        {
+            get
+            {
+                if (IsFiveOfKind) return 10;
+                if (IsFourOfKind) return 9;
+                if (IsFullHouse) return 8;
+                if (ThreeOfKind) return 7;
+                if (TwoPairs) return 6;
+                if (OnePair) return 5;
+                return 1;
+            }
+        }
+
+        private Dictionary<Card, int> GroupedCard =>
+            Cards.GroupBy(it => it)
+                .ToDictionary(it => it.Key, it => it.Count());
+
+        public int CompareTo(Hand? other)
+        {
+            if (other is null) return 1;
+            if (HandValue == other.HandValue)
+                for (var i = 0; i < Cards.Count; i++)
+                {
+                    var card = Cards[i];
+                    var otherCard = other.Cards[i];
+                    if (card.Value != otherCard.Value)
+                        return card.GetSortValue(UseJoker) > otherCard.GetSortValue(UseJoker) ? 1 : -1;
+                }
+
+            return HandValue > other.HandValue ? 1 : -1;
+        }
+
+        private bool CompareUsingJoker(int match)
+        {
+            var start = GroupedCard.Count == 1
+                ? GroupedCard.First().Value
+                : GroupedCard.Where(it => !it.Key.IsJoker).ToList().Max(it => it.Value);
+
+            if (!UseJoker) return start == match;
+            for (var i = start; i <= start + CountJokers; i++)
+                if (i == match)
+                    return true;
+
+            return false;
+        }
+
+        private record Card(char Value, bool IsJoker)
+        {
+            public int GetSortValue(bool useJoker)
+            {
+                if (char.IsNumber(Value)) return int.Parse(Value.ToString());
+                var values = new Dictionary<char, int>
+                {
+                    { 'T', 10 },
+                    { 'J', useJoker ? 0 : 11 },
+                    { 'Q', 12 },
+                    { 'K', 13 },
+                    { 'A', 14 }
+                };
+                return values[Value];
+            }
+        }
+    }
+}
